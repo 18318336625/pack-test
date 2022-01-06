df0bca6f2f886dc98f8718501e4dcf3179359334
Berg.Zou <343144626@qq.com>
2022-01-06 16:51:35 +0800

    测试包

---
+++ src/Service.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QQ
+ * Date: 2022/1/6
+ * Time: 16:41
+ */
+
+namespace Monolog\Monolog;
+
+
+class Service
+{
+    public function index(){
+        echo '这是一个测试包';
+    }
+}
